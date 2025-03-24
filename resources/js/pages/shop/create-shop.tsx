60c460d8d4e542db932d4df29446f934a97fa578
THANETNove <novel_manga@outlook.co.th>
2025-03-24 16:29:04 +0700

    add

--- resources/js/pages/shop/create-shop.tsx
+++ resources/js/pages/shop/create-shop.tsx
@@ -27,7 +27,7 @@ export default function CreateShop() {
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
                 <div className="border-sidebar-border/70 dark:border-sidebar-border relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border md:min-h-min">
                     <div className="mx-auto mt-5 max-w-md rounded p-6 shadow">
-                        <h1 className="mb-4 text-xl font-bold">Create Shop</h1>
+                        <h1 className="mb-4 text-xl font-bold">Create Shop หหห</h1>
                         <form onSubmit={handleSubmit} encType="multipart/form-data">
                             {/* Shop Name */}
                             <div className="mb-4">
